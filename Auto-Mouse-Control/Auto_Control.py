821bf53e4441319db8d07144393f0948fde1d1f8
J. G <it@hgw-tornesch.de>
2021-09-30 15:18:55 +0200

    Removed function definition from while loop ^^"

--- Auto-Mouse-Control/Auto_Control.py
+++ Auto-Mouse-Control/Auto_Control.py
@@ -6,20 +6,20 @@ poss = [[1113, 263], [1511, 309], [929, 314], [1130, 826], [1132, 662], [81, 19]
 dcs = [0]
 sleep_time = 0.8
 
+def click_move(xy, dc = False):
+    mouse.move(xy[0], xy[1])
+    if dc:
+        mouse.double_click()
+    else:
+        mouse.click()
+    time.sleep(sleep_time)
+
 while True:
     inp = input("How many times?\n")
     if not inp.isnumeric:
         exit()
     x = int(inp)
     print("This'll take appr. {} s.\nPlease don't touch the mouse...".format(int(round(len(poss) * sleep_time * x, 0))))
-
-    def click_move(xy, dc = False):
-        mouse.move(xy[0], xy[1])
-        if dc:
-            mouse.double_click()
-        else:
-            mouse.click()
-        time.sleep(sleep_time)
         
     for i in range (x):
         print("{}. run".format(i + 1))
