993dae14a4ea4028af2da8290482b86baf2ac9fb
JG <it@hgw-tornesch.de>
2022-04-27 09:30:21 +0200

    Updated style of lists & conventions

--- AutoControl.py
+++ AutoControl.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
-import mouse, time
+import mouse
+import time
 
 # Points to click
-poss = [[1113, 263], [1511, 309], [929, 314], [1130, 826], [1132, 662], [81, 19], [1895, 20]]
-# Position indexes to double click
-dcs = [0]
+ POSITIONS = [
+    [1113, 263],
+    [1511, 309],
+    [929, 314],
+    [1130, 826],
+    [1132, 662],
+    [81, 19],
+    [1895, 20],
+    ]
+# Position indices to double click
+DOUBLE_CLICK_INDICES = [
+    0,
+    ]
 # Time in seconds between clicks
-sleep_time = 0.8
+SLEEP_TIME = 0.8
 
 def click_move(xy, dc = False):
     mouse.move(xy[0], xy[1])
@@ -15,17 +26,17 @@ def click_move(xy, dc = False):
         mouse.double_click()
     else:
         mouse.click()
-    time.sleep(sleep_time)
+    time.sleep(SLEEP_TIME)
 
 while True:
     inp = input("How many times?\n")
     if not inp.isnumeric:
         exit()
     x = int(inp)
-    print("This'll take appr. {} s.\nPlease don't touch the mouse...".format(int(round(len(poss) * sleep_time * x, 0))))
+    print("This'll take appr. {} s.\nPlease don't touch the mouse...".format(int(round(len(POSITIONS) * SLEEP_TIME * x, 0))))
         
     for i in range (x):
         print("{}. run".format(i + 1))
-        for j in range(len(poss)):
-            click_move(poss[j], j in dcs)
+        for j in range(len(POSITIONS)):
+            click_move(POSITIONS[j], j in DOUBLE_CLICK_INDICES)
     print ("Done.\n")
